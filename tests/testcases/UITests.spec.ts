ff9a57f6ffbdd567b17c26d9e2a2862e03a92f64
Lets Make Automation Easy with Pradeep Nailwal <50335845+PradeepChandNailwal@users.noreply.github.com>
2026-07-17 20:13:16 +0530

    Change test from only to regular test

--- tests/testcases/UITests.spec.ts
+++ tests/testcases/UITests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.waitForTimeout(5000);
   await page.goto('https://playwright.dev/');
 
